b080a1969a40adb47dad1869f660d9eac016b965
kerolosragaie <kerolosragaie@gmail.com>
2023-12-30 19:03:01 +0200

    adding deep linking to details screen

--- app/src/main/java/hoods/com/jetpetrescue/core/navigation/NavGraph.kt
+++ app/src/main/java/hoods/com/jetpetrescue/core/navigation/NavGraph.kt
@@ -7,7 +7,10 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
+import hoods.com.jetpetrescue.core.navigation.RoutesConstants.DETAILS_SCREEN
 import hoods.com.jetpetrescue.core.navigation.RoutesConstants.PET_ID
+import hoods.com.jetpetrescue.core.screens.LoadingScreen
 import hoods.com.jetpetrescue.core.utils.openUrl
 import hoods.com.jetpetrescue.features.home.presentation.screens.details.DetailsScreen
 import hoods.com.jetpetrescue.features.home.presentation.screens.home.HomeScreen
@@ -42,23 +45,33 @@ fun NavGraph(
             arguments = listOf(
                 navArgument(PET_ID) { type = NavType.IntType },
             ),
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "https://www.likander.com/$DETAILS_SCREEN/{$PET_ID}"
+                },
+            ),
         ) { backStackEntry ->
-            val petId = backStackEntry.arguments!!.getInt(PET_ID)
-            val selectedPet = homeViewModel.uiState.animals.data?.get(petId)!!
+            val petId = backStackEntry.arguments?.getInt(PET_ID)!!
+            val selectedPet = homeViewModel.uiState.animals.data?.get(petId)
             val context = LocalContext.current
 
-            DetailsScreen(
-                animal = selectedPet,
-                onClickArrowBack = {
-                    navController.navigateUp()
-                },
-                onClickAdopt = {
-                    openUrl(
-                        context = context,
-                        url = selectedPet.url,
-                    )
-                },
-            )
+            if (selectedPet != null) {
+                DetailsScreen(
+                    animal = selectedPet,
+                    onClickArrowBack = {
+                        navController.navigateUp()
+                    },
+                    onClickAdopt = {
+                        openUrl(
+                            context = context,
+                            url = selectedPet.url,
+                        )
+                    },
+                )
+            } else {
+                LoadingScreen()
+            }
+
         }
     }
 }
